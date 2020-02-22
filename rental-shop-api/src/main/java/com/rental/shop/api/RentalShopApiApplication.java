0550cef0bc7077381c7362b99fbbbcbd387f4df1
Weisberg Barros <weisberg.fontes@nlt.com.br>
2020-02-22 09:36:57 -0300

    created backend project

---
+++ rental-shop-api/src/main/java/com/rental/shop/api/RentalShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.rental.shop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalShopApiApplication.class, args);
+	}
+
+}
